e7d6a235ca1edb7411476cc44ab41d40071a86d1
Vítor Pessoa <40522815+vgpessoa@users.noreply.github.com>
2019-11-15 12:40:52 -0300

    O maior debug que eu já fiz na história
    - Leitura dos arquivos reescrita do zero;
    - Limpeza das variáveis desnecessárias no código;
    - Todos os leaks de memória foram corrigidos;
    - Alguns ajustes adicionais;
    - Correções de muitos bugs;
    - Agora o trabalho finalmente funciona;
    - Adicionei felicidade.

--- TP1/program/main.cpp
+++ TP1/program/main.cpp
@@ -12,8 +12,10 @@ Grupo:
 
 */
 
+#include <bits/stdc++.h> 
 #include <string>
 #include <iostream>
+#include <fstream>
 #include <stdlib.h>
 #include "funcoes.h"
 
@@ -22,12 +24,9 @@ using namespace std;
 int main(){
 
 /* ---------- Criação de Objetos - USUÁRIOS ---------- */
-    vector<vector<string>> linha;    //iniciar o vetor da linha
-    vector<string>::iterator lin; //iterator para linha
 
     vector<Crianca> crianca; //iniciar o vetor de crianças
     vector<Crianca>::iterator itc;
-    vector<int> contCol;
 
     vector<Adulto> adulto; //iniciar o vetor de adultos
     vector<Adulto>::iterator ita;
@@ -36,72 +35,87 @@ int main(){
     vector<Idoso>::iterator itd;
 
 /* ---------- Váriaveis Auxiliares - USUÁRIOS ---------- */
-    int j = 0, k = 0, x = 0, n = 0, tam = 0; //variaveis auxiliares
-    int sizeUsuarios = 0;
-    unsigned int i = 0 ;
 
-    int *depend = new int[1000];                //vetor de id dependentes
+    int *depend = new int[999];                //vetor de id dependentes
     depend[0] = -1;                       // usado para caso extremo de nenhum dependente
-    int *idad = new int[1000];                  //vetor de idades
-    int qcria = 0, qadult = 0, qidos = 0; //quantidade de usuarios
+    int *idad = new int[999];
     int id = 0;
     float saldo = 0.0;
     string nome, categoria;
 
+    // Contadores
+    int qcria = 0, qadult = 0, qidos = 0;
+
 /* ---------- Tratando Entrada - USUÁRIOS ---------- */
-    // FALTA IMPLEMENTAR A LEITURA CORRETAMENTE
-    // OU SEJA, PEDIR PRA LER O NOME DO ARQUIVO QUE VAI SER ABERTO
-    // IMPLEMENTAR DEPOIS QUE TIVERMOS TERMINADO, COISA SIMPLES E RAPIDA
-    // USAR cin pra pedir o nome do arquivo pra ser aberto
+
     std::cout << "Para ler o arquivo, digite o nome de cada arquivo correspondente a seguir, no formato 'arquivo.csv'." << endl;
     std::cout << "O arquivo desejado preferencialmente deve se encontrar na mesma pasta do programa e *deve* usar ; como separador" << endl;
     string file_usuarios;
     std::cout << "Digite o nome do arquivo de USUARIOS que deseja abrir: ";
     std::cin >> file_usuarios;
-    readCsv(linha, file_usuarios,','); // Inicializando entrada de usuários
+    
+    // Abrindo arquivo
+    std::ifstream file_a(file_usuarios);
+    // Varíaveis auxiliáres para os usuários
+    int j = 0, contagem_dependentes = 0;
 
-    // Processando cada linha lida no arquivo
-    for (i = 0; i < linha.size(); i++){
-        j = 0;
-
-        id = atoi (linha[i][j].c_str()); //armazena a id de cada usuario
-        j++;
+    if(file_a.is_open()){
+        // Separar em uma função
+        std::string linha, linha_aux;
+        std::vector<string> tratamento;
         
-        categoria = linha[i][j]; //criança, adulto etc
-        j++;
+        while(getline(file_a, linha)){
+            stringstream X(linha);
+            while(getline(X, linha_aux, ',')){
+                tratamento.push_back(linha_aux);
+            }
 
-        nome = linha[i][j]; //nome do usuario
-        j++;
+            int i = 0;
+            id = stoi(tratamento[i]); i++;
 
-        idad[i] = atoi (linha[i][j].c_str()); //idade do usuario
-        j++;
+            categoria = (tratamento[i]); i++;
 
-        saldo = atof(linha[i][j].c_str());//saldo bancario
-        j++;
+            nome = (tratamento[i]); i++;
 
-        // Armazenando usuários no vector de sua categoria correspondente
-        if(categoria == "crianca"){
-            // Armazenando ID do responsável pela criança no vetor de dependentes
-            depend[k] = atoi(linha[i][j].c_str());
+            idad[j] = stoi(tratamento[i]); i++;
+
+            saldo = stof(tratamento[i]); i++;
+
+            if(categoria == "crianca"){
+                depend[contagem_dependentes] = stoi(tratamento[i]); i++;
+
+                qcria++;
+                Crianca c(id, categoria, nome, idad[j], saldo, depend[contagem_dependentes]);
+                crianca.push_back(c);
+
+                contagem_dependentes++;
+
+            } else if(categoria == "adulto"){
+                qadult++;
+                Adulto a(id, categoria, nome, idad[j], saldo);
+                adulto.push_back(a);
+
+            } else if(categoria == "idoso"){
+                qidos++;
+                Idoso ido(id, categoria, nome, idad[j], saldo);
+                idoso.push_back(ido);
+
+            } else {
+                cout << "Houve um erro na leitura do arquivo, garanta que as classes de usuários foram identificadas corretamente!" << endl;
+                return EXIT_FAILURE;
+            }
+
+
             j++;
 
-            qcria++;
-            Crianca c(id, categoria, nome, idad[j], saldo, depend[k]);
-            crianca.push_back(c);
-            k++;
-        } else if (categoria == "adulto"){
-            qadult++;
-            Adulto a(id, categoria, nome, idad[j], saldo);
-            adulto.push_back(a);
-        } else if (categoria == "idoso"){
-            qidos++;
-            Idoso ido(id, categoria, nome, idad[j], saldo);
-            idoso.push_back(ido);
-        }        
+            tratamento.clear();
+        }
+
+
+    } else {
+        cout << "Erro, nao deu pra abrir!" << endl;
+        return EXIT_FAILURE;
     }
-
-    sizeUsuarios = linha.size();
-
 
 /* ---------- Criação de Objetos - EVENTOS ---------- */
     vector<Cinema> cinema;
@@ -116,135 +130,139 @@ int main(){
     vector<TeatroFantoche> fantoche;
     vector<TeatroFantoche> itfan;
 
-    vector<vector<string>> linha2; // Inicializando o vetor da linha de eventos
     map<double, int> precos;
     map<double, int>::iterator itprecos;
+
 /* ---------- Váriaveis Auxiliares - EVENTOS ---------- */
     int IdEven = 0;
-    int aux = 0;
     string categoriaEven;
     string subcategEven;
     string nomeEven;
-    int *IdDono = new int[1000];
+    int *IdDono = new int[999];
     int NumTipos = 0;
-    int *ingEven = new int[1000];
-    int *ValorEven = new int[1000];
-    int *horarios = new int[1000];
+    int *ingEven = new int[999];
+    int *ValorEven = new int[999];
+    int *horarios = new int[999];
     int quotaIdoso = 0;
     int duracao = 0;
     int horaIni = 0;
     int horaFim = 0;
     int abertura = 0;
-    string *artistas = new string[1000];
+    string *artistas = new string[999];
     // Contadores
     int qBoate = 0, qShow = 0, qCine = 0, qFanto = 0;
     // Atalhos
     int maiorQuota = 0, idMaiorQuota = 0;
-    int p = 0;
+
 /* ---------- Tratando Entrada - EVENTOS ---------- */
     string file_eventos;
     std::cout << "Digite o nome do arquivo de EVENTOS que voce quer abrir: ";
     std::cin >> file_eventos;
 
-    contCol = readCsv(linha2, file_eventos,','); // Inicializando entrada de eventos
+    std::ifstream file_b(file_eventos);
+    int contagem_donos = 0;
 
-    for (i = 0; i < linha2.size(); i++){
-        j = 0;
-        //Capturando os valores para passagem de parametros
-        IdEven = atoi(linha2[i][j].c_str());
-        j++;
-        x++;
-        categoriaEven = linha2[i][j];
-        j++;
-        x++;
-
-        // Se o evento for do tipo cinema, ele não possui subcategoria
-        if(categoriaEven != "cinema"){
-            subcategEven = linha2[i][j];
-            j++;
-            x++;
-        }
-
-        nomeEven = linha2[i][j];
-        j++;
-        x++;
-        IdDono[p] == atoi(linha2[i][j].c_str());
-        j++;
-        x++;
-        p++;
-        NumTipos = atoi(linha2[i][j].c_str());  // Nº de tipos de ingressos diferentes
-        j++;
-        x++;
-
-        for (int k = 0; k < NumTipos; k++){
-            ingEven[k] = atoi(linha2[i][j].c_str()); // Quantidade de ingressos do tipo k
-            j++;
-            x++;
-
-            ValorEven[k] = atof(linha2[i][j].c_str()); // Valor do ingresso do tipo k
-            j++;
-            x++;
-
-            //Contando ingressos por preço
-            precos[ValorEven[k]] += ingEven[k];
-        }
+    if(file_b.is_open()){
+        // Separar em uma função
+        std::string linha, linha_aux;
+        std::vector<string> tratamento2;
         
-        // Se o evento não for do tipo Adulto, a leitura dos horários é realizada
-
-        // Tratando os objetos
-        if(categoriaEven == "infantil"){
-            for (int k = 0; k < linha2[i].size() - (x+1); k++){
-                horarios[k] = atoi(linha2[i][j].c_str());
-                j++;
+        while(getline(file_b, linha)){
+            stringstream Y(linha);
+            while(getline(Y, linha_aux, ',')){
+                tratamento2.push_back(linha_aux);
             }
-            qFanto++;
-            TeatroFantoche fan (horarios, IdEven, nomeEven, IdDono[p], ingEven, ValorEven);
-            fantoche.push_back(fan);
-        } else if(categoriaEven == "cinema"){
-            for (int k = 0; k < linha2[i].size()-(x+1); k++){
-                horarios[k] = stoi(linha2[i][j]);
-                j++;
-            }
+            
+            int i = 0;
+            IdEven = stoi(tratamento2[i]); i++;
 
-            qCine++;
+            categoriaEven = tratamento2[i]; i++;
 
-            duracao = atoi(linha2[i][j].c_str()); // Obtendo a duracao do cinema
-            j++;
-
-            Cinema cine(IdEven, nomeEven, IdDono[p], ingEven, ValorEven, horarios, duracao);
-            cinema.push_back(cine);
-        } else if(categoriaEven == "adulto"){
-            quotaIdoso = atoi(linha2[i][j].c_str()); // Obtendo a quota para idosos do evento
-            j++;
-
-            if(quotaIdoso > maiorQuota){ // Atalho para obter o evento de maior quota
-                maiorQuota = quotaIdoso;
-                idMaiorQuota = IdEven;
+            // Se evento for do tipo cinema, ele não tem subcategoria
+            if(categoriaEven != "cinema"){
+                subcategEven = tratamento2[i]; i++;
             }
 
-            if(subcategEven == "boate"){
-                qBoate++;
-                horaIni = atoi(linha2[i][j].c_str());// Obtendo horario de inicio da boate
-                j++;
-                horaFim = atoi(linha2[i][j].c_str());// Obtendo horario de fim da boate
-                j++;
-                Boate boa(quotaIdoso, horaIni, horaFim, IdEven, nomeEven, IdDono[p], ingEven, ValorEven);
-                boate.push_back(boa);
-            } else if(subcategEven == "show"){
-                qShow++;
-                abertura = atoi(linha2[i][j].c_str());// Obtendo horario de abertura do show 
-                j++;
-                int w = 0;
-                for(; j <= linha2[i].size(); j++){
-                    artistas[w] = linha2[i][j]; // Obtendo listagem dos artistas que apresentarão no show
-                    w++;
+            nomeEven = tratamento2[i]; i++;
+
+            IdDono[contagem_donos] = stoi(tratamento2[i]); i++;
+
+            NumTipos = stoi(tratamento2[i]); i++;
+
+            for(int aux_tipos = 0; aux_tipos < NumTipos; aux_tipos++){
+                ingEven[aux_tipos] = stoi(tratamento2[i]); i++;
+
+                ValorEven[aux_tipos] = stof(tratamento2[i]); i++;
+
+                precos[ValorEven[aux_tipos]] += ingEven[aux_tipos];
+            }
+
+            // Se o evento não for do tipo Adulto, a leitura dos horários é realizada
+            if(categoriaEven == "infantil"){
+                for(int aux_h = 0; aux_h < tratamento2.size() - i; aux_h++){
+                    horarios[aux_h] = stoi(tratamento2[i]); i++;
                 }
-                Show sho(quotaIdoso, abertura, artistas, IdEven, nomeEven, IdDono[p], ingEven, ValorEven);
-                show.push_back(sho);
+
+                qFanto++;
+                TeatroFantoche fan(horarios, IdEven, nomeEven, IdDono[contagem_donos], ingEven, ValorEven);
+                fantoche.push_back(fan);
+
+            } else if(categoriaEven == "cinema"){
+                for(int aux_h = 0; aux_h < tratamento2.size() - (i+1); aux_h++){
+                    horarios[aux_h] = stoi(tratamento2[i]); i++;
+                }
+                
+                duracao = stoi(tratamento2[i]); i++;
+
+                qCine++;
+                Cinema cine(IdEven, nomeEven, IdDono[contagem_donos], ingEven, ValorEven, horarios, duracao);
+                cinema.push_back(cine);
+
+            } else if(categoriaEven == "adulto"){
+                quotaIdoso = stoi(tratamento2[i]); i++;
+
+                if(quotaIdoso > maiorQuota){
+                    maiorQuota = quotaIdoso;
+                    idMaiorQuota = IdEven;
+                }
+
+                if(subcategEven == "boate"){
+                    horaIni = stoi(tratamento2[i]); i++;
+                    horaFim = stoi(tratamento2[i]); i++;
+
+                    qBoate++;
+                    Boate boa(quotaIdoso, horaIni, horaFim, IdEven, nomeEven, IdDono[contagem_donos], ingEven, ValorEven);
+                    boate.push_back(boa);
+                } else if(subcategEven == "show"){
+                    abertura = stoi(tratamento2[i]); i++;
+
+                    int aux_arts = 0;
+                    for(; i <= tratamento2.size(); i++){
+                        artistas[aux_arts] = tratamento2[i];
+                        aux_arts++;
+                    }
+                    
+                    qShow++;
+                    Show sho(quotaIdoso, abertura, artistas, IdEven, nomeEven, IdDono[contagem_donos], ingEven, ValorEven);
+                    show.push_back(sho);
+                }
+
+
+            } else {
+                cout << "Houve um erro na leitura do arquivo, garanta que as classes de usuários foram identificadas corretamente!" << endl;
+                return EXIT_FAILURE;
+
             }
+
+
+            contagem_donos++;
+
+            tratamento2.clear();
         }
-        
-        x = 0;
+
+    } else {
+        cout << "Erro, nao deu pra abrir!" << endl;
+        return EXIT_FAILURE;
     }
 
 /* ---------- Saída do Programa ---------- */
@@ -253,35 +271,29 @@ int main(){
 //   0.1 - Número de crianças, adultos e idosos
     std::cout << endl << "Número de usuários:" << endl;
     std::cout << "Crianças: " << qcria << endl;
-    std::cout << "Adultos: " << qadult << endl;
+    std::cout << "Adultos: " << qadult + qidos << endl;
     std::cout << "Idosos: " << qidos << endl << endl;
 
 //   0.2 - Idade mínima, máxima e média para todos os usuários
     IdadeDosUsuarios(idad, j);
 
 //   0.3 - Número mínimo, médio e máximo de dependentes por adulto
-    if (depend[0] == -1){ // Atalho para o caso onde não há nenhum dependente na entrada
-        std::cout << "Número de dependentes:" << endl;
-        std::cout << "Minima: " << 0 << endl;
-        std::cout << "Máxima: " << 0 << endl;
-        std::cout << "Média: " << 0 << endl;
-    } else {
-        NumeroDependentes(depend, qcria, qadult + qidos);
-    }
+    NumeroDependentes(depend, qcria, qadult + qidos);
+
 
 //   0.4 - Lista de dependentes por adulto
     std::cout << "Dependentes:" << endl;
     for(itc = crianca.begin(); itc != crianca.end();itc++){
         for(ita = adulto.begin(); ita != adulto.end(); ita++){
             if(itc->get_id_responsavel() == ita->get_id()){
-                std::cout << ita->get_nome() << " (ID: " << ita->get_id() << "): " << itc->get_nome() << " (ID: " << itc->get_id_responsavel() << "): " << endl;
+                std::cout << ita->get_nome() << " (ID: " << ita->get_id() << "): " << itc->get_nome() << " (ID: " << itc->get_id_responsavel() << "), " << endl;
         }
     }}
 
     for(itc = crianca.begin(); itc != crianca.end();itc++){
         for(itd = idoso.begin(); itd != idoso.end(); itd++){
             if(itc->get_id_responsavel() == itd->get_id()){
-                std::cout << itd->get_nome() << " (ID: " << itd->get_id() << "): " << itc->get_nome() << " (ID: " << itc->get_id_responsavel() << "): " << endl;
+                std::cout << itd->get_nome() << " (ID: " << itd->get_id() << "): " << itc->get_nome() << " (ID: " << itc->get_id_responsavel() << "), " << endl;
         }
     }}
 
@@ -300,47 +312,55 @@ int main(){
     std::cout << "Número de eventos que o usuário possui:" << endl;
     //Criação de map para armazenar numero eventos por dono
     map<int, int> dono;
-    map<int, int>::iterator it;
+    map<int, int>::iterator itdono;
 
     //Passagem para o Map
-    for(int i = 0; i < p; i++){
-        dono[IdDono[i]]++;
+    for(int d = 0; d < contagem_donos; d++){
+        dono[IdDono[d]]++;
     }
-     for(it = dono.begin(); it != dono.end(); it++){
+    for(itdono = dono.begin(); itdono != dono.end(); itdono++){
         for(ita = adulto.begin(); ita != adulto.end(); ita++){
-            if(it->first == ita->get_id()){
-                std::cout<<ita->get_nome()<<" (ID: "<<ita->get_id()<<"): "<<it->second<<endl;
+            if(itdono->first == ita->get_id()){
+                std::cout << ita->get_nome() << " (ID: " << ita->get_id() << "): " << itdono->second << endl;
+            }
+        }
+     }
+     for(itdono = dono.begin(); itdono != dono.end(); itdono++){
+        for(itd = idoso.begin(); itd != idoso.end(); itd++){
+            if(itdono->first == itd->get_id()){
+                std::cout << itd->get_nome() << " (ID: " << itd->get_id() << "): " << itdono->second << endl;
             }
         }
      }
 
 //   1.3 - Nome e ID do evento com a maior cota para idosos
-    std::cout << "Evento com maior cota para idoso:" << endl;
+    std::cout << endl << "Evento com maior cota para idoso:" << endl;
     for(itshow = show.begin(); itshow != show.end(); itshow++){
         if(itshow->get_id() == idMaiorQuota){
-                std::cout << itshow->get_nome() << " (ID: " << itshow->get_id() << "): " << "maiorQuota" << endl;
+                std::cout << itshow->get_nome() << " (ID: " << itshow->get_id() << "): " << maiorQuota << endl;
         }
     }
     for(itboate = boate.begin(); itboate != boate.end(); itboate++){
         if(itboate->get_id() == idMaiorQuota){
-                std::cout << itboate->get_nome() << " (ID: " << itboate->get_id() << "): " << "maiorQuota" << endl;
+                std::cout << itboate->get_nome() << " (ID: " << itboate->get_id() << "): " << maiorQuota << endl;
         }
     }
 
 
 //   1.4 - Número total de bilhetes de cada valor
-    std::cout << "Número de ingressos por preço:" << endl;
+    std::cout << endl << "Número de ingressos por preço:" << endl;
     for(itprecos = precos.begin(); itprecos != precos.end(); itprecos++){
         std::cout << "R$" << itprecos->first << ": " << itprecos->second << endl;
     }
 
 
-    delete [] depend ;
-    delete [] idad ;
-    delete [] ingEven ;
-    delete [] ValorEven ;
-    delete [] horarios ;
+    delete [] depend;
+    delete [] idad;
+    delete [] ingEven;
+    delete [] ValorEven;
+    delete [] horarios;
     delete [] IdDono;
+    delete [] artistas;
 
     return 0;
 }
--- TP1/src/funcoes.cpp
+++ TP1/src/funcoes.cpp
@@ -1,123 +1,77 @@
 #include "funcoes.h"
+#include <algorithm>
 
-// 1. Função que divide as linhas
-std::vector<std::string> splitLine(std::string line, char sep, int &cont){
-
-    // Convertendo a string para stream string
-    std::istringstream split(line);
-
-    // Criando um vetor para a linha
-    std::vector<std::string> lineVector;
-
-    // Divide a linha para o separador de caracteres
-    for (std::string each; std::getline(split, each,sep); lineVector.push_back(each));
-
-    // Conta o número de colunas
-    cont = lineVector.size();
-
-    return lineVector;
-}
-
-
-// 2. Função que abre arquivo csv e realiza sua leitura
-std::vector<int> readCsv( std::vector<std::vector<std::string>> &resutlCsv, std::string nameFile, char sep){
-
-    std::string lineString;
-    std::ifstream myfile(nameFile);
-    std::vector<int> contCol;
-    int cont;
-
-    // Verificando se o arquivo foi aberto corretamente
-    if(myfile.is_open()){
-
-        // Processo de leitura linha por linha
-        while(getline(myfile, lineString)){
-            cont = 0;
-            resutlCsv.push_back(splitLine(lineString,sep,cont));
-            contCol.push_back(cont);
-        }
-        myfile.close();
-
-    } else {
-        std::cout << "Erro! Não foi possível abrir esse arquivo" << endl;
-        std::cout << "Finalizando programa..." << endl;
-    }
-
-    return contCol;
-}
-
-// 3. Função que imprime a idade dos usuarios segundo o especificado
+// 1. Função que imprime a idade dos usuarios segundo o especificado (vetor idades, quantidade de usuarios)
 void IdadeDosUsuarios(int *idad, int max){
     int aux = 0, soma = 0;
     float resul = 0.0;
 
-    for (int i = 0; i < max; i++) { 
-        soma += idad[i];
+    for (int s = 0; s < max; s++) { 
+        soma += idad[s];
     }
 
-    for (int i = 0; i < max; i++) { 
-        for (int j = i; j < max; j++){ 
-            if (idad[i] < idad[j]){ 
-                aux = idad[i];
-                idad[i] = idad[j];
-                idad[j] = aux;
-            } 
-        } 
-   }
-   
-   resul = (float)soma/max;
+    std::sort(idad, idad + max);
 
-    //Impressao das idades minima, maxima e media
+    resul = (float)soma/max;
+
+    // Impressao das idades minima, maxima e media
     std::cout << "Idade dos usuários:" << endl;
     std::cout << "Minima: "<< idad[0] << endl;
     std::cout << "Máxima: "<< idad[max-1] << endl;
     std::cout << "Média: "<< resul << endl << endl;
 }
 
-// 4. Numero de dependentes por adulto
-void NumeroDependentes (int *depend, int qp, int np){
-    //Criação de map para armazenar numero de dependentes por adulto
-    map<int, int> numdep;
-    map<int, int>::iterator it;
+// 2. Numero de dependentes por adulto (vetor dependentes, quantidade criancas, quantidade adultos)
+void NumeroDependentes(int *depend, int qp, int np){
 
-    //Variaveis axuiliares
-    int maior, menor, soma = 0;
-    float resul;
-    int max = sizeof(depend);
+    if(depend[0] == -1){
+        std::cout << "Número de dependentes:" << endl;
+        std::cout << "Minima: " << 0 << endl;
+        std::cout << "Máxima: " << 0 << endl;
+        std::cout << "Média: " << 0 << endl;
+    } else {
+        // Criação de map para armazenar numero de dependentes por adulto
+        map<int, int> numdep;
+        map<int, int>::iterator it;
 
-    //Passagem para o Map
-    for(int i = 0; i < max; i++){
-        numdep[depend[i]]++;
-    }
-    
-    //Definindo o minimo e o maximo
-    it = numdep.begin();
-    maior = it->second;
-    menor = it->second;
+        // Variaveis auxiliares
+        int maior, menor, soma = 0;
+        float resul;
 
-    for(it = numdep.begin(); it != numdep.end(); it++){
-        if(maior < it->second){
-            maior = it->second;
+        // Passagem para o Map
+        for(int i = 0; i < qp; i++){
+            numdep[depend[i]]++;
         }
-        if(menor > it->second){
-            menor = it->second;
+        
+        // Definindo o minimo e o maximo
+        it = numdep.begin();
+        maior = it->second;
+        menor = it->second;
+
+        for(it = numdep.begin(); it != numdep.end(); it++){
+            if(maior < it->second){
+                maior = it->second;
+            }
+            if(menor > it->second){
+                menor = it->second;
+            }
         }
+
+        if(np > numdep.size()){ // Se quantidade de adultos total != quantidade de adultos com crianças
+            menor = 0;
+        }
+
+        for(it = numdep.begin(); it != numdep.end(); it++){
+            soma += it->second;
+        }
+
+        //Tirando a media
+        resul = (float)soma/np;
+
+        //Impressao
+        std::cout << "Número de dependentes:" << endl;
+        std::cout << "Minima: "<< menor << endl;
+        std::cout << "Máxima: "<< maior << endl;
+        std::cout << "Média: "<< resul << endl << endl;
     }
-
-    if(np > numdep.size()){ // Se quantidade de adultos total != quantidade de adultos com crianças
-        menor = 0;
-    }
-
-    for(it = numdep.begin(); it != numdep.end(); it++){
-        soma = soma + it->second;
-    }
-
-    //Tirando a media
-    resul = (float)soma/np;
-
-    //Impressao
-    std::cout << "Número de dependentes:" << endl;
-    std::cout << "Minima: "<< menor << endl;
-    std::cout << "Máxima: "<< maior << endl;
-    std::cout << "Média: "<< resul << endl << endl;
 }
